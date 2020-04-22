64010c711c0bd77c8676579907627ada9d53d657
Pyl <pyl.jan.batu@gmail.com>
2020-04-22 22:37:14 +0800

    test case

---
+++ phonebook/phonebook.py
@@ -0,0 +1,8 @@
+
+class PhoneBook():
+
+    def add(self, name, number):
+        pass
+
+    def lookup(self, name):
+        pass
---
+++ phonebook/test_phonebook.py
@@ -0,0 +1,10 @@
+import unittest
+from phonebook import PhoneBook
+
+class PhoneBookTest(unittest.TestCase):
+
+    def test_lookup_by_name(self):
+        phonebook = PhoneBook()
+        phonebook.add('Pyl', '123456')
+        number = phonebook.lookup('Pyl')
+        self.assertEqual(number, '123456')
