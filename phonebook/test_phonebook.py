7229ef607bd4570084675a8f68d5166aaf7d26f4
Pyl <pyl.jan.batu@gmail.com>
2020-04-22 22:51:35 +0800

    skip annotation

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -3,18 +3,19 @@ from phonebook import PhoneBook
 
 
 class PhoneBookTest(unittest.TestCase):
-    def test_lookup_by_name(self):
+
+    def setUp(self) -> None:
         phonebook = PhoneBook()
+
+    def test_lookup_by_name(self):
         phonebook.add('Pyl', '123456')
         number = phonebook.lookup('Pyl')
         self.assertEqual(number, '123456')
 
     def test_missing_name(self):
-        phonebook = PhoneBook()
         with self.assertRaises(KeyError):
             phonebook.lookup('missing')
 
     @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
-        phonebook = PhoneBook()
         self.assertTrue(phonebook.is_consistent())
